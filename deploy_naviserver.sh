c0384d64fd3c97f0b69a53a0acc03b42e0be88ae
Maksym Zinchenko <siqsuruq@gmail.com>
2019-10-12 17:05:09 -0100

    Update deploy_naviserver.sh
    Added git Ubuntu package

--- deploy_naviserver.sh
+++ deploy_naviserver.sh
@@ -18,7 +18,7 @@ function update_ubuntu () {
 	echo -e "${BL}------------------ Updating Ubuntu: ------------------{NC}"
 	export DEBIAN_FRONTEND=noninteractive
 	apt-get update
-	apt-get -y install unzip tcl tcl-dev tcllib tdom tcl-tls libssl-dev libpng-dev libpq-dev automake postgresql postgresql-contrib nsf nsf-shells fortune mc file
+	apt-get -y install unzip tcl tcl-dev tcllib tdom tcl-tls libssl-dev libpng-dev libpq-dev automake postgresql postgresql-contrib nsf nsf-shells fortune mc file git
 	echo -e "${GR}------------------ Done$ ------------------{NC}"
 }
 
